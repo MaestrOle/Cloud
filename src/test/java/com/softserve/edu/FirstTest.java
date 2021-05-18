d1408030316246933658b365a3ca4f631f56a85e
Oleg Kravtcov <71754272+MaestrOle@users.noreply.github.com>
2021-05-18 12:13:01 +0300

    Add files via upload

--- src/test/java/com/softserve/edu/FirstTest.java
+++ src/test/java/com/softserve/edu/FirstTest.java
@@ -8,10 +8,10 @@ public class FirstTest {
     @Test
     public void checkOne() {
         System.out.println("FirstTest::checkOne()");
-        System.out.println("THYMELEAF_CONNECTION"=" + System.getenv("THYMELEAF_CONNECTION")");
-        System.out.println("THYMELEAF_USER"=" + System.getenv("THYMELEAF_USER")");
-        System.out.println("THYMELEAF_PASSWORD"=" + System.getenv("THYMELEAF_PASSWORD")");
-        System.out.println("JENKINS_VAR"=" + System.getenv("JENKINS_VAR")");
+        System.out.println("THYMELEAF_CONNECTION = " + System.getenv("THYMELEAF_CONNECTION"));
+        System.out.println("THYMELEAF_USER = " + System.getenv("THYMELEAF_USER"));
+        System.out.println("THYMELEAF_PASSWORD = " + System.getenv("THYMELEAF_PASSWORD"));
+        System.out.println("JENKINS_VAR = " + System.getenv("JENKINS_VAR"));
         Assertions.assertTrue(true);
     }
 
